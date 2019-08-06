c0c15f7b612a56dba749bc1d651c3a99754ef69a
Marek <jonas.wu@waystorm.com>
2019-08-06 23:14:41 +0800

    change database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controller/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-aerodynamic-96354',
-        user: 'marekwu',
-        password: '',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: 'true'
     }
 });
 
